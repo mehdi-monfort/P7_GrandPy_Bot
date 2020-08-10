a4dbc5fb7aa74513e6a37a160adf8c1472bf7df2
mehdi-monfort <mehdimonfort@gmail.com>
2020-08-10 18:34:51 +0200

    wiki module creation

---
+++ app/wiki/wiki.py
@@ -0,0 +1,38 @@
+import requests
+from pprint import pprint
+
+url = "https://fr.wikipedia.org/w/api.php"
+ 
+latitude = 48.856614
+longitude = 2.3522219
+ 
+params = {
+    "format": "json",
+    "action": "query",
+    "list": "geosearch",
+    "gsradius": 10000,
+    "gscoord": f"{latitude}|{longitude}"
+}
+
+response = requests.get(url, params=params)
+response.raise_for_status()
+
+geosearch_data = response.json()
+
+page_id = geosearch_data['query']['geosearch'][0]['pageid']
+
+params = {
+    "format": "json",
+    "action": "query",
+    "prop": "extracts|info",
+    "inprop": "url",
+    "explaintext": 1,
+    "pageids": page_id
+}
+
+result = requests.get(url, params=params)
+result.raise_for_status()
+
+response = result.json()
+pprint(response)
+print(response['query']['pages'][str(page_id)]['extract'])
