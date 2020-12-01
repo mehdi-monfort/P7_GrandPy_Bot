d32c13ed2c50edee2c1543e5108fa67554fcb876
mehdi-monfort <mehdimonfort@gmail.com>
2020-12-01 08:25:48 +0100

    deployement heroku

--- app/config/settings.py
+++ app/config/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "hello",
 ]
 
 MIDDLEWARE = [
@@ -63,13 +62,6 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
